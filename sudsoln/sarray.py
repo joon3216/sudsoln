9591dcaab69b2cfe4902a61c49c3e57431a2b448
Junkyu Park <joon3216@gmail.com>
2019-07-23 03:13:16 -0400

    Array now supports slice.

--- sudsoln/sarray.py
+++ sudsoln/sarray.py
@@ -8,36 +8,48 @@ class Array():
         Initialize the Array object.
 
         >>> test = [
-                [1, 2, 3, 4], 
-                [5, 2, 0, 3], 
-                [7, 5, 1, 0], 
-                [2, 6, 2, 4]
-            ]
+        ...     [1, 2, 3, 2], 
+        ...     [5, 2, 0, 4], 
+        ...     [7, 5, 1, 0],
+        ... ]
+        ...
         >>> test = Array(test)
+        >>> test.show
+        [['1', '2', '3', '2'], ['5', '2', '0', '4'], ['7', '5', '1', '0']]
+        >>> test.flat
+        ['1', '2', '3', '2', '5', '2', '0', '4', '7', '5', '1', '0']
+        >>> test.nrow
+        3
+        >>> test.ncol
+        4
+        >>> test.shape
+        (3, 4)
+        >>> test.size
+        12
         '''
 
         mkstr = lambda x: str(x) if type(x) != str else x
-        flat = []
+        lena = len(array)
         ncols = []
-        for i in range(len(array)):
+        for i in range(lena):
             ncols.append(len(array[i]))
             array[i] = list(map(mkstr, array[i]))
-            flat.extend(array[i])
-        ncols_is_uniform = (len(set(ncols)) == 1)
+        if len(set(ncols)) != 1:
+            raise ValueError(
+                'All sublists of array should have the same length.'
+            )
 
         self.show = array
-        self.flat = flat
-        self.nrow = len(array)
-        self.ncol = max(ncols)
-        if ncols_is_uniform:
-            self.shape = (self.nrow, self.ncol)
-        else:
-            self.shape = (self.nrow, )
+        self.nrow = lena
+        self.ncol = ncols[0]
+        self.flat =\
+            [array[i][j] for i in range(lena) for j in range(self.ncol)]
+        self.shape = (self.nrow, self.ncol)
         self.size = sum(ncols)
 
 
     def __getitem__(self, key):
-        '''(Array, (int, int)) -> object
+        '''(Array, (int, int) or slices) -> object or Array
 
         Return the entry of self at key.
         
@@ -50,34 +62,68 @@ class Array():
         ...
         >>> test = Array(test)
         >>> test[(0, 1)]
-        '5'
+        '2'
         >>> test[2, 3]
         '0'
+        >>> test[2, :]
+        Array([
+        ['7', '5', '1', '0']
+        ])
+        >>> test[:, 3]
+        Array([
+        ['4', '3', '0', '4']
+        ])
+        >>> test[(1, 3), :]
+        Array([
+        ['5', '2', '0', '3'],
+        ['2', '6', '2', '4']
+        ])
+        >>> test[:2, ] # subject to unittest: Raises IndexError
+        Array([
+        ['1', '2', '3', '4'],
+        ['5', '2', '0', '3']
+        ])
+        >>> test[:, :2]
+        Array([
+        ['1', '2'],
+        ['5', '2'],
+        ['7', '5'],
+        ['2', '6']
+        ])
+        >>> test[::2, ::2]
+        Array([
+        ['1', '3'],
+        ['7', '1']
+        ])
+        >>> test[::3, ::2]
+        Array([
+        ['1', '3'],
+        ['2', '2']
+        ])
         '''
         
-        return self.show[key]
+        key0_is_slice = isinstance(key[0], slice)
+        key0_is_int   = isinstance(key[0], int)
+        key1_is_slice = isinstance(key[1], slice)
+        key1_is_int   = isinstance(key[1], int)
+        
+        if key0_is_slice and key1_is_slice:
+            lz_inner = list(zip(*self.show[key[0]]))[key[1]]
+            lz = [list(row) for row in zip(*lz_inner)]
+            return Array(lz)
+        elif key0_is_slice and key1_is_int:
+            cols = [list(row) for row in list(zip(*self.show))]
+            return Array([cols[key[1]][key[0]]])
+        elif key0_is_int and key1_is_slice:
+            return Array([self.show[key[0]][key[1]]])
+        elif key0_is_int and key1_is_int:
+            return self.show[key[0]][key[1]]
+        else:
+            print('key0 is tuple ', isinstance(key[0], tuple))
+            raise TypeError('Invalid argument type asdf')
 
 
-    def __list__(self):
-        '''(Array) -> list
-
-        Return the list representation of self.
-
-        >>> test = [
-        ...     [1, 2, 3], 
-        ...     [5, 2, 0], 
-        ...     [7, 5, 1]
-        ... ]
-        ...
-        >>> test = Array(test)
-        >>> list(test)
-        [['1', '2', '3'], ['5', '2', '0'], ['7', '5', '1']]
-        '''
-
-        return self.show
-
-
-    def __repr__(self, key):
+    def __repr__(self):
         '''(Array) -> str
 
         Return the Array representation of self.
@@ -98,15 +144,48 @@ class Array():
         ])
         '''
 
-        headline, midline, endline = 'Array([', '', '])'
-        
+        headline, midline, endline = 'Array([\n', '', '])'
+        for i in range(len(self.show)):
+            if i != len(self.show) - 1:
+                midline += str(self.show[i]) + ',\n'
+            else:
+                midline += str(self.show[i]) + '\n'
+        return headline + midline + endline
 
 
     def __setitem__(self, key, value):
         '''(Array, (int, int), object) -> None
 
         Assign value to self's key.
+
+        >>> test = [
+        ...     [1, 2, 3, 4], 
+        ...     [5, 2, 0, 3], 
+        ...     [7, 5, 1, 0], 
+        ...     [2, 6, 2, 4]
+        ... ]
+        ...
+        >>> test = Array(test)
+        >>> test[0, 0] = 3
+        >>> test
+        Array([
+        ['3', '2', '3', '4'],
+        ['5', '2', '0', '3'],
+        ['7', '5', '1', '0'],
+        ['2', '6', '2', '4']
+        ])
+        >>> test[(3, 2)] = '.'
+        >>> test
+        Array([
+        ['3', '2', '3', '4'],
+        ['5', '2', '0', '3'],
+        ['7', '5', '1', '0'],
+        ['2', '6', '.', '4']
+        ])
         '''
+
+        self.show[key[0]][key[1]] = str(value)
+
 
     def copy(self):
         '''(Array) -> Array
@@ -114,11 +193,13 @@ class Array():
         Return the deep copy of self.
         '''
 
+        return Array([row[:] for row in self.show])
+
 
     def flatten(self):
         '''(Array) -> list of str
 
-        Return the flatten version of self.
+        Return the flattened version of self.
 
         >>> test = [
         ...     [1, 2, 3], 
@@ -131,23 +212,82 @@ class Array():
         ['1', '2', '3', '5', '2', '0', '7', '5', '1']
         '''
 
-        return self.flat
+        return list(self.flat)
 
 
-    def itemset(self):
-        '''(Array) -> None
+    def itemset(self, key, value):
+        '''(Array, (int, int), object) -> None
         
-        Itemset
+        Mutate key number of self to value; this is exactly the same as 
+        .__setitem__().
+
+        >>> test = [
+        ...     [1, 2, 3, 4], 
+        ...     [5, 2, 0, 3], 
+        ...     [7, 5, 1, 0], 
+        ...     [2, 6, 2, 4]
+        ... ]
+        ...
+        >>> test = Array(test)
+        >>> test.itemset((3, 3), '6')
+        >>> test
+        Array([
+        ['1', '2', '3', '4'],
+        ['5', '2', '0', '3'],
+        ['7', '5', '1', '0'],
+        ['2', '6', '2', '6']
+        ])
         '''
 
-    def reshape(self):
-        '''(Array) -> Array
+        self[key] = value
 
-        Reshape
+
+    def reshape(self, key0, key1):
+        '''(Array, int, int) -> Array
+
+        Reshape self to have key0 rows and key1 columns if applicable.
+        
+        >>> test = [
+        ...     [1, 2, 3, 4], 
+        ...     [5, 2, 0, 3], 
+        ...     [7, 5, 1, 0], 
+        ...     [2, 6, 2, 4]
+        ... ]
+        ...
+        >>> test = Array(test)
+        >>> test.reshape(8, 2)
+        Array([
+        ['1', '2'],
+        ['3', '4'],
+        ['5', '2'],
+        ['0', '3'],
+        ['7', '5'],
+        ['1', '0'],
+        ['2', '6'],
+        ['2', '4']
+        ])
+        >>> test.reshape(2, 8)
+        Array([
+        ['1', '2', '3', '4', '5', '2', '0', '3'],
+        ['7', '5', '1', '0', '2', '6', '2', '4']
+        ])
         '''
 
-    def shape(self):
-        '''(Array) -> (int, int)
+        if self.size != key0 * key1:
+            raise ValueError(
+                'cannot reshape the Array of size ' + str(self.size) +\
+                ' into shape (' + str(key0) + ', ' + str(key1) + ')'
+            )
+        result = [[] for i in range(key0)]
+        k = 0
+        for i in range(key0):
+            for j in range(key1):
+                result[i].append(self.flatten()[k])
+                k += 1
+        return Array(result)
 
-        Shape of self.
-        '''
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
