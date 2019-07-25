2c0956149aae2c46e005e7d4433fdd6e1ea2eb60
Junkyu Park <joon3216@gmail.com>
2019-07-25 15:38:54 -0400

    Added all submodules

--- sudsoln/__init__.py
+++ sudsoln/__init__.py
@@ -1,4 +1,8 @@
 
 from sudsoln.sudoku import *
+import sudsoln.appearance as appearance
+import sudsoln.candidate as candidate
 import sudsoln.questions as questions
+import sudsoln.sarray as sarray
+import sudsoln.union as union
 __version__ = '0.0.13'
