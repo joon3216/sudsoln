6bc779408266982f42c4cef9d7e120350688e050
Junkyu Park <joon3216@gmail.com>
2019-07-24 21:01:25 -0400

    Initialized the structure of appearance.py

--- sudsoln/appearance.py
+++ sudsoln/appearance.py
@@ -1,1 +1,20 @@
 
+class Appearance():
+    '''Appearance collection.'''
+
+    def __init__():
+        '''(Appearance) -> None
+
+        Initialize Appearance object.
+        '''
+
+    def sieve():
+        '''(Appearance) -> None
+
+        Sieve.
+        '''
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
