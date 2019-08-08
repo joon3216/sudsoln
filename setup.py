21cc2144efc74acd657635caf78f1df69595fddf
Junkyu Park <joon3216@gmail.com>
2019-08-08 18:56:43 -0400

    ver 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sudsoln",
-    version="0.0.21",
+    version="0.1.0",
     author="Junkyu Park",
     author_email="joon3216@gmail.com",
     description="A n ** 2-by-n ** 2 sudoku solver.",
@@ -14,9 +14,9 @@ setuptools.setup(
     url="https://github.com/joon3216/sudsoln",
     packages=setuptools.find_packages(),
     python_requires='>=3',
-    # install_requires=['numpy'],
     license="MIT License",
     classifiers=[
+        "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
